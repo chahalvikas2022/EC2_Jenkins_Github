7958dc456dcda2b58798234bd9314770b12f1fba
chahalvikas2022 <chahalvikas2022@gmail.com>
2024-06-01 20:40:22 +0530

    Jenkins deployments in EC2 instance

---
+++ main.tf
@@ -0,0 +1,104 @@
+# Define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Create a Public Subnet
+resource "aws_subnet" "my_public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+# Create a Route Table
+resource "aws_route_table" "my_public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
+# Associate the Route Table with the Public Subnet
+resource "aws_route_table_association" "my_public_rt_assoc" {
+  subnet_id      = aws_subnet.my_public_subnet.id
+  route_table_id = aws_route_table.my_public_rt.id
+}
+
+# Create a Security Group
+resource "aws_security_group" "my_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create an EC2 Instance
+resource "aws_instance" "my_instance" {
+  ami           = "ami-00beae93a2d981137"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.my_public_subnet.id
+  security_groups = [aws_security_group.my_sg.id]
+  
+  tags = {
+    Name = "MyPublicInstance"
+  }
+
+  associate_public_ip_address = true
+
+ # key_name = "your-key-name"  # Replace with your key pair name
+ # User data script to install Jenkins
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y java-1.8.0-openjdk
+              wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+              rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins.io.key
+              yum install -y jenkins
+              service jenkins start
+              chkconfig jenkins on
+              EOF
+
+
+}
+
+# Output the instance's public IP
+output "instance_public_ip" {
+  value = aws_instance.my_instance.public_ip
+}
